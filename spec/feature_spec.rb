b5ee2d239f3f4994034475c5f4ce963d4aefb43c
Arif Ebrahim <>
2021-05-06 20:02:40 +0100

    step 5: added top_up

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -9,6 +9,13 @@ it 'should have an initial balance of 0' do
   expect(card.balance).to eq 0
 end
 
-
+  # In order to keep using public transport
+  # As a customer
+  # I want to add money to my card
+it 'should allow users to top up' do
+  card = Oystercard.new
+  card.top_up(50)
+  expect(card.balance).to eq 50
+end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Oystercard do
   it 'should have an initial balance of 0'  do
     expect(subject.balance).to eq(0)
   end
+
+  describe '#top_up' do
+    it 'should allow users to increase the balance' do
+      expect { subject.top_up(50) }.to change{ subject.balance }.by 50
+    end
+  end
 end
